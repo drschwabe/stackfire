053fc33ac9727314b01e609d761b28c3e494dc00
Derrick Schwabe <admin@nerdbusiness.com>
2016-06-16 11:38:04 -0600

    Implement ar.disable based on new signature feature. Let's you target a specific command path and disable it. For overriding listeners other modules have defined higher up the stack. Theoretically useful if you did not author the original module that defined said listener.

--- async-router.js
+++ async-router.js
@@ -6,52 +6,57 @@ var ar = { routes : [] }
 
 ar.listen = function(param1, callback) {
   //param1: a string or an array of strings.
+  //(is either a single path or array of paths)  
 
   var that = this
-  var registerRoute = function(path, callback) {
+  var registerRoute = function(path, listenerCallback) {
 
-    var route = routeParser(path)
+    var route = routeParser(trimSigFromPath(path))
     var existingRoute = _.find(that.routes, function(existingRoute) {
       return existingRoute.route.spec == route.spec
     })
 
+    //The newMiddleware contains two properties; one is the callback
+    //the other is the full path (containing sig) so we can later target/override this. 
+    var newMiddleware = { func : listenerCallback, path: path }    
+
     //Determine if the route already exists:
     if(!existingRoute) {
+      var route = { route: route, middleware: [newMiddleware] }
       //Make an entry for it; add to known routes and define middleware array/stack:
-      that.routes.push({ route: route, middleware: [callback]})  
+      that.routes.push(route)  
     } else {
-      //If the route already exists, just push this module's callback 
-      //into the middleware array/stack: 
-      existingRoute.middleware.push(callback)
+      //If the route already exists, just push the new middleware into the 
+      //existing stack: 
+      existingRoute.middleware.push(newMiddleware)
     }      
   }
 
-  //Parse first paramater: 
-  //(is either a single route or array of routes)
-  if(_.isArray(param1)) {
-    param1.forEach(function(path) {
-      registerRoute(path, callback)
-    })
-  } else {
+  //If an array was not supplied, create an array anyway: 
+  var paths
+  if(!_.isArray(param1)) paths = [param1]
+  else paths = param1
+
+  paths.forEach(function(path) {
     //Check for signature: 
-    if(param1.charAt(0) == '@') {
+    if(path.charAt(0) == '@') {
       //Register it with two padding functions that set and clear the signature
       //on the state object.  
-      registerRoute(trimSigFromPath(param1), function(state, next) {
+      registerRoute(path, function(state, next) {
         state.sig = snipSignature(param1) //< Stamps signature.
         next(null, state)
       })
       //The actual function: 
-      registerRoute(trimSigFromPath(param1), callback)
+      registerRoute(path, callback)
       //Clear the signature: 
-      registerRoute(trimSigFromPath(param1), function(state, next) {
+      registerRoute(path, function(state, next) {
         delete state.sig
         next(null, state)        
       })
-      return
+    } else {
+      registerRoute(path, callback)
     }
-    registerRoute(param1, callback)
-  }
+  })
 }
 
 ar.fire = function(path, state, callback) {
@@ -83,7 +88,8 @@ ar.fire = function(path, state, callback) {
   async.series([
     function(seriesCallback) {
       //Fire any middleware (route agnostic)...
-      if(that.middleware) { //Seed it with the req and state:   
+      if(that.middleware) { 
+        //Seed it with the req and state:   
         that.middleware[0] = function(next) { next(null, state) }
         //Run the middleware stack: 
         async.waterfall(that.middleware, function(err, state) {
@@ -96,17 +102,22 @@ ar.fire = function(path, state, callback) {
     }, 
     function(seriesCallback) {
       var seedFunction = function(next) { next(null, state) }
-      if(matchingRoute) {
+      if(matchingRoute) {      
         //Give the waterfall a seed function with null error, parsed/matched route (req), and state: 
         if(!matchingRoute.seeded) { //but only if we haven't already done it: 
-          matchingRoute.middleware.unshift(seedFunction)      
+          matchingRoute.middleware.unshift({func: seedFunction })      
           matchingRoute.seeded = true      
         } else { //If already seeded, we overwrite the original seed function
           //(because req and state may have changed): 
-          matchingRoute.middleware[0] = seedFunction
+          matchingRoute.middleware.func = seedFunction
         }
 
-        async.waterfall(matchingRoute.middleware, function(err, state) {
+        //Create a copy of the middleware stack we are about to run
+        //containing only the functions
+        //(preparing the data structure for what async.waterfall will expect): 
+        var middlewareToRun = _.map(matchingRoute.middleware, function(entry) { return entry.func })
+
+        async.waterfall(middlewareToRun, function(err, state) {
           if(err) return console.log(err)
           if(_.isFunction(callback)) callback(null, state)
         })
@@ -126,6 +137,14 @@ ar.use = function(callback) {
   //this is replaced by a seed function when ar.fire is called.
   this.middleware.push(callback)
   //^ push the callback to the stack.
+}
+
+ar.disable = function(path) {
+  //Disable the supplied command based on it's unique path.
+  //Expecting @name-of-module/actual-command-path
+  var targetRoute = _.find(this.routes, function(route) { return route.route.spec == trimSigFromPath(path) })
+  var targetListeners = _.filter(targetRoute.middleware, function(listener) { return listener.path == path })
+  targetRoute.middleware = _.difference(targetRoute.middleware, targetListeners)
 }
 
 //Helper functions: 

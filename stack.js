1ebd8888f5e7c72d9f82b67c609412e1bff23fb7
Derrick Schwabe <admin@nerdbusiness.com>
2019-11-19 23:36:03 -0800

    Strip down stack.on, and write basic test to check it returns a basic data model for a listener (also new in stack4 that stack.on returns a listener)

--- stack.js
+++ stack.js
@@ -4,13 +4,32 @@ const async = require('async')
 const gg = require('gamegrids')
 
 const stack = {
+  commands : [], //< list of commands invoked via stack.fire
+  params : {}, //reference to the params of the live command/listener in progress
+  //this is updated when the live command changes; ie- column change
   listeners : [],
-  commands : [],
-  grid : gg.populateCells(gg.createGrid(1,1)),
+  grid : gg.populateCells(gg.createGrid(1,1))
 }
 
-require('./mods/on.js')
+//### stack.on
+//Establishes a listener that will wait in stack.listeners until it is matched and then queued into a command via stack.fire
+require('./mods/on.js')(stack)
 
+//### stack.fire
+//establishes a new command instance and fires the appropriate chain of sorted listeners, asynchronously.
 //require('./mods/fire.js')
+
+//### stack.match
+//matches listeners to a given pattern; returns all matching listeners; called by stack.fire
+stack.match
+
+//### stack.sort
+
+
+
+stack.parentCommand = () => {
+  //shortcut to access the parent command of current command/listener in progress. ex:
+  //stack.parentCommand().params.body
+}
 
 module.exports = stack
---
+++ tests/stack.on-outputs-listener/client.js
@@ -0,0 +1,6 @@
+const stack = require('../../stack.js')
+const test = require('tape-catch')
+
+console.log('hello flat world')
+
+require('./test.js')(test, stack)

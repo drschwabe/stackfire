f18c741df480a9e127c5a57ff060a339d1870297
Derrick Schwabe <admin@nerdbusiness.com>
2020-01-08 09:29:35 -0700

    Emit new internal event when a listener is invoked (corresponds to stackfire-tools 177fd37182cbd7bc712601b277ea9dc535373e46)

--- mods/loop.js
+++ mods/loop.js
@@ -5,6 +5,7 @@ module.exports = (stack) => {
   stack.loop = (command, callback) => {
     stack.utils.forEach((util) => util('stack.loop_started', command))
     async.eachSeries(command.listener_instances, (listenerInstance, eachSeriesCallback) => {
+      stack.utils.forEach((util) => util('stack.listener_invoked', listenerInstance))
       if(listenerInstance.async) {
         eachSeriesCallback.end = () => eachSeriesCallback(true) //< exit the loop early
         eachSeriesCallback.fire = (...params) => stack.fire(...params, eachSeriesCallback )

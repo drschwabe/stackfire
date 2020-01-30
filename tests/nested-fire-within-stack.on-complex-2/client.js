1b8d33b885fb6e700fe888311357d5b054bcb762
Derrick Schwabe <admin@nerdbusiness.com>
2020-01-30 11:07:16 -0700

    Remove unneeded client side code testing (to stackfire-tools tests instead)

--- tests/nested-fire-within-stack.on-complex-2/client.js
+++
@@ -1,8 +0,0 @@
-const Stack = require('../../stack.js')
-const stack = new Stack()
-
-const stackTools = require('stackfire-tools')
-stackTools.inject(stack)
-
-const test = require('tape-catch')
-require('./test.js')(test, stack)

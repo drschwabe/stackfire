21a751546460df55b6b5b08d21e30c0bd2d4100b
Derrick Schwabe <admin@nerdbusiness.com>
2020-02-07 07:55:00 -0700

    Initial 'native pause' working currently on sync functions, failing / needed to disable buffer function for this to work - so next commit should update it so this can work on an async func too (without buffer)

--- mods/buffer.js
+++ mods/buffer.js
@@ -4,6 +4,6 @@ module.exports = (stack) => {
   stack.buffer = (...params) => {
     //TODO offer a 'path' param that would let you set the buffer to only execute on a given command string
     //for now stack only supports one buffer
-    stack.buffer_func = _.find(params, (param) => _.isFunction(param))
+    //stack.buffer_func = _.find(params, (param) => _.isFunction(param))
   }
 }
--- mods/loop.js
+++ mods/loop.js
@@ -7,7 +7,7 @@ module.exports = (stack) => {
     stack.utils.forEach((util) => util('stack.loop_started', command))
     async.eachSeries(command.listener_instances, (listenerInstance, eachSeriesCallback) => {
       stack.utils.forEach((util) => util('stack.listener_invoked', listenerInstance))
-      if(listenerInstance.async) {
+      if(listenerInstance.async ) {
         eachSeriesCallback.command = command
         eachSeriesCallback.end = () => eachSeriesCallback(true) //< exit the loop early
         eachSeriesCallback.fire = (...params) => {
@@ -16,7 +16,15 @@ module.exports = (stack) => {
         }
         return listenerInstance.func(eachSeriesCallback)
       }
-      //otherwise it's a sync function so just run it and call eachCallback immediately:
+      //it's a sync function ...
+      if(stack.pausing) { //but if we are pausing a sync func,
+        stack.unpause = () => { //this unpause func will call...
+          listenerInstance.func() //< the sync func...
+          eachSeriesCallback() //< and the async callback immediately after...
+        }//to advance the loop!
+        return
+      }
+      //otherwise just run them now:
       listenerInstance.func()
       return eachSeriesCallback()
     }, () => {

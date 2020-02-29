7202e847c93559f50b51a8d580d85636f2b9ae1e
Derrick Schwabe <admin@nerdbusiness.com>
2020-02-29 12:02:34 -0700

    Refactor wrap mod to accommodate for lib functions... parse params for the function and optionally, an object key value pair in the case of a lib function like myLib.myFunc() also make a custom stack path to said function ie- helloWorld() becomes available as stack.fire('hello-world') or in the case of a lib func like hello.world() it becomes avail as stack.fire('hello/world')

--- mods/wrap.js
+++ mods/wrap.js
@@ -1,11 +1,33 @@
 //feed a function and wrap it...
 const _ = require('underscore')
+const _s = require('underscore.string')
+
+function camelCaseToDash (str) {
+  return str.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase()
+}
 
 module.exports = stack => {
 
-  stack.wrap = functionToWrap => {
+  stack.wrap = (...params) => {
+    //parse params for the function and optionally, an object key value pair in the case of a lib function like myLib.myFunc()
+    //also make a custom stack path to said function ie- helloWorld() becomes available as stack.fire('hello-world')
+    //or in the case of a lib func like hello.world() it becomes avail as stack.fire('hello/world')
+    let functionToWrap
+    let stackPath
+    if(_.isFunction(params[0])) {
+      functionToWrap = params[0]
+      stackPath = camelCaseToDash(functionToWrap.name).split('.').join('/')
+    } else if(_.isObject(params[0]) && _.isString( params[1] ) ) {
+      //user called:  stack.wrap( obj, 'obj.functionName')
+      let objName = _s.strLeft( params[1], '.' )
+      let funcName = _s.strRight(params[1], '.' )
+      functionToWrap = params[0][funcName]
+      stackPath = camelCaseToDash(params[1]).split('.').join('/')
+    } else {
+      return console.error('invalid params')
+    }
 
-    stack.on(  functionToWrap.name , () => {
+    stack.on( stackPath , () => {
       if(functionToWrap.wrapping) return
       functionToWrap(stack.body()) //< we supply stack.body as param as way to transfer the params in this context
       //ie- stack.fire(functionToWrap.name, 'some extra param' ) //< stack.body() is 'some extra param'
@@ -15,17 +37,17 @@ module.exports = stack => {
 
       let executeFunc = () => func(...params)
       //if we are already in a create-doc/save fire (ie- fired from another place in app) just run the func:
-      if(stack.command && stack.command.path == functionToWrap.name) return executeFunc()
+      if(stack.command && stack.command.path == stackPath) return executeFunc()
       //otherwise we invoke the fire on a one-time listener:
       func.wrapping = true
-      stack.once(functionToWrap.name, executeFunc, 50)
+      stack.once(stackPath, executeFunc, 50)
       //if a command is in progress use next otherwise fire direct:
       if(stack.queue && stack.queue.length) {
-        stack.next.fire(functionToWrap.name, () => {
+        stack.next.fire(stackPath, () => {
           func.wrapping = false
         })
       } else {
-        stack.fire(functionToWrap.name, () => {
+        stack.fire(stackPath, () => {
           func.wrapping = false
         })
       }
--- tests/wrap/test.js
+++ tests/wrap/test.js
@@ -2,22 +2,53 @@
 module.exports = (test, stack) => {
 
   test('Can wrap a normal function in a fire', t => {
-    t.plan(5)
+    t.plan(4)
+
+    let listenerCount = 0
 
     let helloWorld = () => {
-      console.log('hello world')
-      t.pass('helloWorld original function ran')
+      console.log('helloWorld original function ran')
+      listenerCount++
     }
 
     helloWorld = stack.wrap(helloWorld)
 
-    stack.on('helloWorld', () => {
-      t.pass('helloWorld listener ran')
+    console.log('hello.world() ...')
+    helloWorld()  //< invokes 1 listener
+    t.equals(  listenerCount , 1, 'holloWorld() called after stack.wrap(helloWorld) invokes')
+
+    listenerCount = 0
+    console.log('stack.fire("hello-world") ...')
+    stack.fire('hello-world')  //< invokes just the original function since above we are listening for the camel case one
+    t.equals(  listenerCount , 1, 'stack.fire("hello-world")invokes the stack.wrap(helloWorld) original function')
+
+    const hello = {}
+    hello.world = () => {
+      console.log('hello.world original function ran')
+      listenerCount++
+    }
+
+    stack.on('hello/world', () => {
+      console.log('hello/world listener ran')
+      listenerCount++
     })
 
-    helloWorld()  //< invokes 2 listeners
-    stack.fire('helloWorld') //< invokes 2 listeners
-    stack.fire('hello-world')  //< invokes just the original function since above we are listening for the camel case one
+    //hello.world = stack.wrap(hello.world, 'hello/world')
+    hello.world = stack.wrap(hello, 'hello.world')
+
+    listenerCount = 0
+
+    console.log('hello.world() ...')
+    hello.world()
+
+    t.equals(  listenerCount , 2, 'hello.world() called after stack.wrap(hello, "hello.world") invokes')
+    //^ you must currently provide the stack path of manual
+
+    listenerCount = 0
+
+    console.log('stack.fire("hello/world") ...')
+    stack.fire('hello/world')
+    t.equals(  listenerCount , 2, 'stack.fire("hello/world") called after stack.wrap(hello, "hello.world") invokes')
 
   })
 
